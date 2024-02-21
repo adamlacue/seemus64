089de42a4c40947cee29fce3d9d78cc50cab1547
bkletz18 <112676159+bkletz18@users.noreply.github.com>
2024-02-21 12:11:36 -0500

    wef

--- index.php
+++ index.php
@@ -59,52 +59,17 @@ if(isset($_REQUEST["activity"])) {
             break;
 
                 case "FILE-CREATE-PROCESS": 
-                    $target_dir = "/file_uploads";
-                    $target_file = $target_dir . basename($_FILES["file"]["name"]);
-                    $uploadOk = 1;
-                    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-                    
-                    // Check if image file is a actual image or fake image
-                    if(isset($_POST["submit"])) {
-                      $check = getimagesize($_FILES["file"]["tmp_name"]);
-                      if($check !== false) {
-                        echo "File is an image - " . $check["mime"] . ".";
-                        $uploadOk = 1;
-                      } else {
-                        echo "File is not an image.";
-                        $uploadOk = 0;
-                      }
-                    }
-                    
-                    // Check if file already exists
-                    if (file_exists($target_file)) {
-                      echo "Sorry, file already exists.";
-                      $uploadOk = 0;
-                    }
-                    
-                    // Check file size
-                    if ($_FILES["file"]["size"] > 5000000) {
-                      echo "Sorry, your file is too large.";
-                      $uploadOk = 0;
-                    }
-                    
-                    // Allow certain file formats
-                    if($imageFileType != "pdf" && $imageFileType != "png" && $imageFileType != "jpeg"
-                    && $imageFileType != "gif" ) {
-                      echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-                      $uploadOk = 0;
-                    }
-                    
-                    // Check if $uploadOk is set to 0 by an error
-                    if ($uploadOk == 0) {
-                      echo "Sorry, your file was not uploaded.";
-                    // if everything is ok, try to upload file
-                    } else {
-                      if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
-                        echo "The file ". htmlspecialchars( basename( $_FILES["file"]["name"])). " has been uploaded.";
-                      } else {
-                        echo "Sorry, there was an error uploading your file.";
-                      }
+                    if (count($_FILES) > 0) {
+                        if (is_uploaded_file($_FILES['file']['tmp_name'])) {
+                            $imgData = file_get_contents($_FILES['file']['tmp_name']);
+                            $imgType = $_FILES['file']['type'];
+                            $sql = "INSERT INTO tbFiles(fdFileType ,fdFile) VALUES(:FileType, :FileData)";
+                            $statement = $conn->prepare($sql);
+
+                            $statement->bindParam('Filetype', $imgType, $PDO::PARAM_INT);
+                            $statement->bindParam('FileData', $imgType, $PDO::PARAM_INT);
+                            $current_id = $statement->execute() or die("<b>Error:</b> Problem on Image Insert<br/>" . mysqli_connect_error());
+                        }
                     }
                 
                     
