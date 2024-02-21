8d294abc66bf05ca250fc3aaf82ffe6d6e9a23dc
bkletz18 <112676159+bkletz18@users.noreply.github.com>
2024-02-21 14:35:41 -0500

    asfd

--- include/top.inc.php
+++ include/top.inc.php
@@ -37,24 +37,3 @@ try {
   }
   
 ?>
-  
-  
-  
-
-// try {
-
-//     $conn->beginTransaction();
-
-//     $conn->exec("INSERT INTO MyGuests (firstname, lastname, email) 
-//     VALUES ('John3', 'Doe', 'john@example.com')");
-//     $conn->exec("INSERT INTO MyGuests (firstname, lastname, email) 
-//     VALUES ('Mary', 'Moe', 'mary@example.com')");
-//     $conn->exec("INSERT INTO MyGuests (firstname, lastname, email) 
-//     VALUES ('Julie', 'Dooley', 'julie@example.com')");
-    
-//     $conn->commit();
-//     echo "\nNew record created successfully";
-//   } catch(PDOException $e) {
-//     echo $sql . "<br>" . $e->getMessage();
-//   }
-
