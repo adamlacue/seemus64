39c65d626f1af5a20f43f00df30b9b0d41c8f15c
bkletz18 <112676159+bkletz18@users.noreply.github.com>
2024-02-21 15:19:49 -0500

    asd

--- view.php
+++ view.php
@@ -4,22 +4,16 @@ include "class/Utils.class.php";
 
 $activity = formRequest("activity");
 
-$sql = "SELECT id, fdFile, fdFilename, fdFilesType, fdFilesSize, fdDateTime, fdArchive FROM `tbFiles` WHERE id = " . formRequest("id");
+$sql = "SELECT id,fdFile, fdFilename,fdFileType,fdFileSize,fdDateTime,fdArchive FROM `tbFiles` WHERE id = " . formRequest("id");
 
 $stmt = $conn->prepare($sql);
 $stmt->execute();
 $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
 
 // Check if $result has anything in it or not (Returns a FALSE if no data in there).
-if ($result) {
+if($result) {
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        // Set the correct Content-Type header
-        header("Content-Type: " . $row["fdFilesType"]);
-
-        // Set the Content-Disposition header if you want the browser to prompt for download
-        header("Content-Disposition: attachment; filename=\"" . $row["fdFilename"] . "\"");
-
-        // Output the binary data
+        header("Content-Type: " . $row["fdFileType"]);
         echo $row["fdFile"];
     }
 }
