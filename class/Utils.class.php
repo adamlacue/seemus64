c74a5558a3cee11fb754caa1222f4b98ec870a24
bkletz18 <112676159+bkletz18@users.noreply.github.com>
2024-02-21 14:22:38 -0500

    asd

--- class/Utils.class.php
+++ class/Utils.class.php
@@ -1,8 +1,8 @@
 <?php
 
 class Utils {
-
-    public static function prettyPrint($sqlStmt) {
+    
+    public static function prettyPrint($sqlStmt) { 
         global $conn;
         $stmt = $conn->prepare($sqlStmt);
         $stmt->execute();
@@ -12,21 +12,21 @@ class Utils {
         $printedHeader=false;
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
             if(!$printedHeader) {
-            echo "<table>";   // Start Table
+            echo "<table border=1>";   // Start Table
             echo "<tr>";      // Start Header Row
-                foreach($row as $col_name => $val)
+            foreach($row as $col_name => $val)
                 {
                 echo "<th>$col_name</th>";    // Print Each Field Name
                 }
             echo "</tr>";
             $printedHeader=true;
             }
-        echo "<tr>"; // Start Header Row
-        foreach($row as $col_name => $val)
-        {
-        echo "<td>$val</td>"; // Print Each Field Name
-        }
-        echo "</tr>"; // Start Header Row
+            echo "<tr>";      // Start Header Row
+            foreach($row as $col_name => $val)
+            {
+            echo "<td>$val</td>";    // Print Each Field Name
+            }
+            echo "</tr>";      // Start Header Row
         }
         echo "</table>";
         } else {
@@ -34,4 +34,5 @@ class Utils {
         }
     }
 }
+
 ?>
