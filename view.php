2799b6b3806df239b268475a705c6ad0233aa4fb
bkletz18 <112676159+bkletz18@users.noreply.github.com>
2024-02-21 19:05:06 -0500

    asd

--- view.php
+++ view.php
@@ -13,6 +13,7 @@ $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
 // Check if $result has anything in it or not (Returns a FALSE if no data in there).
 if($result) {
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        ob_clean();
         header("Content-Type: " . $row["fdFileType"]);
         echo $row["fdFile"];
     }
